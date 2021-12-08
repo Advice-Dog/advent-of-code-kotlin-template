ed33b0974f2c8bc047a0946cdc783b373eafb0f3
Chris <chrisporter0111@gmail.com>
2021-12-08 02:03:25 -0800

    Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,23 +1,56 @@
 fun main() {
 
-    fun part1(input: Bingo): Int {
-        input.numbers.forEach {
+    fun part1(input: List<String>): Int {
+        val board = input.toBoard()
+        board.numbers.forEach {
+            println("Calling number: $it")
 
+            board.call(it)
+
+            val bingo = board.boards.find { it.hasBingo() }
+            if (bingo != null) {
+                println("BINGO")
+                val score = bingo.getScore() * it
+                println("score: $score")
+                return score
+            }
         }
 
         return -1
     }
 
+    fun part2(input: List<String>): Int {
+        val board = input.toBoard()
+
+        val boards = board.boards.toMutableList()
+        var score = -1
+
+        board.numbers.forEach {
+            println("Calling number: $it")
+            println("boards: ${boards.size}, score: $score")
+            board.call(it)
+            val bingos = boards.filter { it.hasBingo() }
+            if (bingos.isNotEmpty()) {
+                for (bingo in bingos) {
+                    println("BINGO\n$bingo")
+                    score = bingo.getScore() * it
+                    boards.remove(bingo)
+                }
+            }
+        }
+
+
+        return score
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-        .toBoard()
-
     check(part1(testInput) == 4512)
+    check(part2(testInput) == 1924)
 
     val input = readInput("Day04")
-        .toBoard()
-
     println(part1(input))
+    println(part2(input))
 }
 
 fun List<String>.toBoard(): Bingo {
@@ -49,8 +82,54 @@ fun List<String>.toBoard(): Bingo {
     return Bingo(numbers, list)
 }
 
-data class Bingo(val numbers: List<Int>, val boards: List<Board>)
+data class Bingo(val numbers: List<Int>, val boards: List<Board>) {
+    fun call(number: Int) {
+        boards.forEach { it.call(number) }
+    }
+}
 
-data class Board(val positions: List<List<BoardPosition>>)
+data class Board(val positions: List<List<BoardPosition>>) {
+    fun call(number: Int) {
+        positions.forEach { it.forEach { it.call(number) } }
+    }
 
-data class BoardPosition(val value: Int, val hasBeenCalled: Boolean = false)
+    fun hasBingo(): Boolean {
+        val any = positions.any { it.all { it.hasBeenCalled } }
+        if (any)
+            return true
+
+        for (i in 0..4)
+            if (positions.all { it.elementAt(i).hasBeenCalled }) {
+                return true
+            }
+
+        return false
+    }
+
+    fun getScore(): Int {
+        return positions.flatten()
+            .filter { !it.hasBeenCalled }
+            .sumOf { it.value }
+    }
+
+    override fun toString(): String {
+        return positions.joinToString(separator = "\n") {
+            it.joinToString(separator = " ") { it.toString() }
+        }
+        //return super.toString()
+    }
+}
+
+data class BoardPosition(val value: Int, var hasBeenCalled: Boolean = false) {
+    fun call(number: Int) {
+        if (value == number)
+            hasBeenCalled = true
+    }
+
+    override fun toString(): String {
+        return if (hasBeenCalled)
+            "."
+        else
+            return value.toString()
+    }
+}
